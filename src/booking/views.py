c2148f8c011474755c7cf8bef2ec1b75bb8ad63c
Michael <37400755+micha31r@users.noreply.github.com>
2021-03-19 23:02:40 +1300

    Edited things

--- src/booking/views.py
+++ src/booking/views.py
@@ -4,5 +4,19 @@ from .models import Activity
 def activities_view(request):
 	ctx = {}
 	ctx["qs"] = Activity.objects.all().order_by("child_price")
+	if request.method == "POST":
+		data = request.POST
+		adult_count = data.get("adult_count", None)
+		child_count = data.get("child_count", None)
+		product_id = data.get("product_id", None)
+		if adult_count and child_count and product_id:
+			cart = request.session.get("cart", [])
+			cart.append({
+				"id":product_id,
+				"adult_count":adult_count,
+				"child_count":child_count,
+			})
+			request.session["cart"] = cart
+			print(cart)
 	template_file = "booking/activities.html"
 	return render(request, template_file, ctx)
