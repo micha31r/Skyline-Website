8f9da51a0c0315bb7ccc391c434b5e678ee4e6b8
Michael <37400755+micha31r@users.noreply.github.com>
2021-03-20 17:36:53 +1300

    Edited checkout process

--- src/booking/forms.py
+++ src/booking/forms.py
@@ -1,1 +1,113 @@
+import datetime
 from django import forms
+from usermgmt.models import Profile
+
+class UserInfoForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['first_name', 'last_name', 'email', 'phone']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['first_name'].widget.attrs.update({'placeholder': 'First Name'})
+		self.fields['first_name'].label = "First Name"
+		self.fields['last_name'].widget.attrs.update({'placeholder': 'Last Name'})
+		self.fields['last_name'].label = "Last Name"
+		self.fields['email'].widget.attrs.update({'placeholder': 'example@example.com'})
+		self.fields['email'].label = "Email Address"
+		self.fields['phone'].widget.attrs.update({'placeholder': '(+64)(0) 12 345 6789'})
+		self.fields['phone'].label = "Phone Number"
+
+	date = forms.DateTimeField(
+		widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Date",
+	            "type": "date"
+	        }
+	    ),
+	    label="Arrival Date"
+	)
+	date_repeat = forms.DateTimeField(
+		widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Repeat Date",
+	            "type": "date"
+	        }
+	    ),
+	    label="Repeat Arrival Date"
+	)
+
+	def clean(self):
+		data = super().clean()
+		fn = data.get("first_name")
+		ln = data.get("last_name")
+		ph = data.get("phone")
+		ad = data.get("date")
+		rad = data.get("date_repeat")
+		if fn and ln and ph and ad and rad:
+			if not fn.isalpha():
+				self._errors["first_name"] = ["First name can only contain A-z and less than 65 letters"]
+			if not ln.isalpha():
+				self._errors["last_name"] = ["Last name can only contain A-z and less than 129 letters"]
+			phone = ph.replace("+","")
+			if (len(phone) > 12 or len(phone) < 9) or not phone.isnumeric():
+				self._errors["phone"] = ["Phone number should be between 9-12 digits and only contain 0-9 and +"]
+			if ad.date() <= datetime.date.today():
+				self._errors["date"] = ["Arrival date must be a future date"]
+			elif ad != rad:
+				self._errors["date_repeat"] = ["Arrival dates must match"]
+		else:
+			self._errors["first_name"] = ["Invalid form data!"]
+		return data
+
+class PaymentForm(forms.Form):
+	first_name = forms.CharField(
+	    widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"First Name"
+	        },
+	    ),
+	    label="First Name"
+	)
+	last_name = forms.CharField(
+	    widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Last Name"
+	        },
+	    ),
+	    label="Last Name"
+	)
+	email = forms.EmailField(
+	    widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Email"
+	        },
+	    ),
+	    label="Email Adress"
+	)
+	phone = forms.CharField(
+	    widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Phone Number"
+	        }
+	    ),
+	    label="Phone Number"
+	)
+	date = forms.DateTimeField(
+		widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Phone Number",
+	            "type": "date"
+	        }
+	    ),
+	    label="Arrival Date"
+	)
+	date_repeat = forms.DateTimeField(
+		widget=forms.TextInput(
+	        attrs={
+	            "placeholder":"Phone Number",
+	            "type": "date"
+	        }
+	    ),
+	    label="Repeat Arrival Date"
+	)
--- src/booking/views.py
+++ src/booking/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Activity
+from .forms import UserInfoForm, PaymentForm
 
 def activities_view(request):
 	ctx = {}
 	ctx["qs"] = Activity.objects.all().order_by("child_price")
+	ctx["cart"] = cart = request.session.get("cart", [])
 	if request.method == "POST":
 		data = request.POST
 		adult_count = data.get("adult_count", None)
 		child_count = data.get("child_count", None)
 		product_id = data.get("product_id", None)
 		if adult_count and child_count and product_id:
-			cart = request.session.get("cart", [])
 			obj = Activity.objects.get(product_id=product_id)
 			cart.append({
 				"name":obj.name,
@@ -49,3 +52,54 @@ def cart_view(request):
 			request.session["cart"] = cart
 	template_file = "booking/cart.html"
 	return render(request, template_file, ctx)
+
+def checkout_step1_view(request):
+	ctx = {}
+	ctx["cart"] = cart = request.session.get("cart", [])
+	ctx["card_items_count"] = len(cart)
+	ctx["total"] = 0
+	for i in range(len(cart)):
+		item = cart[i]
+		cart[i]["total"] = total = item["adult_price"] * float(item["adult_count"]) + item["child_price"] * float(item["child_count"])
+		ctx["total"] += total
+	if request.method == "POST":
+		form = UserInfoForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			info = {
+				"first_name":data.get("first_name"),
+				"last_name":data.get("last_name"),
+				"email":data.get("email"),
+				"phone":data.get("phone"),
+				"date":data.get("date"),
+				"date_repeat":data.get("date_repeat"),
+			}
+			request.session["user_info"] = json.dumps(info, default=str)
+			return redirect("booking:checkout-step2")
+	else:
+		form = UserInfoForm()
+	ctx["form"] = form
+	template_file = "booking/checkout_step1.html"
+	return render(request, template_file, ctx)
+
+def checkout_step2_view(request):
+	ctx = {}
+	ctx["cart"] = cart = request.session.get("cart", [])
+	ctx["card_items_count"] = len(cart)
+	ctx["total"] = 0
+	for i in range(len(cart)):
+		item = cart[i]
+		cart[i]["total"] = total = item["adult_price"] * float(item["adult_count"]) + item["child_price"] * float(item["child_count"])
+		ctx["total"] += total
+	if request.method == "POST":
+		form = PaymentForm(request.POST)
+	else:
+		form = PaymentForm()
+	ctx["form"] = form
+	template_file = "booking/checkout_step2.html"
+	return render(request, template_file, ctx)
+
+
+
+
+
