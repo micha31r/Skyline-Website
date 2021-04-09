feecd894b319aa6fd95b34ab052e71b9bb267a9a
Michael <37400755+micha31r@users.noreply.github.com>
2021-04-09 14:45:55 +1200

    Edited things

--- src/booking/admin_views.py
+++ src/booking/admin_views.py
@@ -9,11 +9,24 @@ from .models import Activity, Ticket
 
 @method_decorator(login_required, name='dispatch')
 class BookingListView(ListView):
-    model = Ticket
-    paginate_by = 50
-    template_name = 'booking/admin/booking_list.html'
+	model = Ticket
+	paginate_by = 10
+	template_name = 'booking/admin/booking_list.html'
 
-    def dispatch(self, request, *args, **kwargs):
-    	if self.request.user.is_staff:
-    		return super().dispatch(request, *args, **kwargs)
-    	else: return Http404()
+	def dispatch(self, request, *args, **kwargs):
+		if self.request.user.is_staff:
+			return super().dispatch(request, *args, **kwargs)
+		else: return Http404()
+
+	def get_context_data(self, **kwargs):
+		ctx = super(BookingListView, self).get_context_data(**kwargs)
+		qs = list(ctx["object_list"])
+		# ctx["total"] = 0
+		for i in range(len(qs)):
+			item = qs[i]
+			total = item.activity.adult_price * float(item.adult_count) + item.activity.child_price * float(item.child_count)
+			qs[i].total = total
+			# ctx["total"] += total
+		return ctx
+
+    
