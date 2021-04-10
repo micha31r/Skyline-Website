5558d9c2f89577d0aaaac46c665d40b0aefcef8b
Michael <37400755+micha31r@users.noreply.github.com>
2021-04-10 23:33:06 +1200

    Update admin_views.py

--- src/booking/admin_views.py
+++ src/booking/admin_views.py
@@ -17,31 +17,37 @@ def booking_list_view(request):
 	ctx = {}
 	all_qs = Ticket.objects.all()
 	# Filter queryset
+	# Store and get search parameter from session
+	s = request.session
 	if request.POST:
 		p = request.POST
-		activity = p.getlist("activity")
-		issue_date = p.get("issue-date")
-		arrival_date = p.get("arrival-date")
-		wildcard = p.get("wildcard")
-		if activity:
-			all_qs = all_qs.filter(activity__product_id__in=activity)
-		if issue_date:
-			all_qs = all_qs.filter(timestamp=issue_date)
-		if arrival_date:
-			all_qs = all_qs.filter(activation_date=arrival_date)
-		if wildcard:
-			lookups = Q(user__first_name__icontains=wildcard) | \
-				Q(user__last_name__icontains=wildcard) | \
-				Q(activity__name__icontains=wildcard) | \
-				Q(activity__product_id=wildcard) | \
-				Q(code=wildcard)
-			if wildcard.isdigit() and len(wildcard) < 3:
-				lookups |= Q(adult_count=wildcard) | \
-					Q(child_count=wildcard)
-			all_qs = all_qs.filter(lookups)
+		s["activity"] = p.getlist("activity")
+		s["issue-date"] = p.get("issue-date")
+		s["arrival-date"] = p.get("arrival-date")
+		s["wildcard"] = p.get("wildcard")
+	activity = s.get("activity", [])
+	issue_date = s.get("issue-date", None)
+	arrival_date = s.get("arrival-date", None)
+	wildcard = s.get("wildcard", None)
+	if activity:
+		all_qs = all_qs.filter(activity__product_id__in=activity)
+	if issue_date:
+		all_qs = all_qs.filter(timestamp=issue_date)
+	if arrival_date:
+		all_qs = all_qs.filter(activation_date=arrival_date)
+	if wildcard:
+		lookups = Q(user__first_name__icontains=wildcard) | \
+			Q(user__last_name__icontains=wildcard) | \
+			Q(activity__name__icontains=wildcard) | \
+			Q(activity__product_id=wildcard) | \
+			Q(code=wildcard)
+		if wildcard.isdigit() and len(wildcard) < 3:
+			lookups |= Q(adult_count=wildcard) | \
+				Q(child_count=wildcard)
+		all_qs = all_qs.filter(lookups)
 	ctx["total_result_count"] = all_qs.count()
 
-	paginate_by = 10
+	paginate_by = 30
 	paginator = Paginator(all_qs, paginate_by)
 	ctx["page_obj"] = paginator.page(request.GET.get('page', 1)) # Set current page
 	ctx["page_range"] = range(1, ctx["page_obj"].paginator.num_pages+1)
