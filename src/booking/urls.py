eb3406d9b0d9f735326ac90d9660d206204bef32
Michael <37400755+micha31r@users.noreply.github.com>
2021-04-19 17:58:29 +1200

    Edited things
    Added luge (activity description) page

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from . import activity_views
 from . import admin_views
 
 app_name = "booking"
 
 urlpatterns = [
+    # Booking pages
     path('activities', views.activities_view, name="activities"),
     path('cart', views.cart_view, name="cart"),
     path('checkout/step1', views.checkout_step1_view, name="checkout-step1"),
@@ -12,7 +14,10 @@ urlpatterns = [
     path('checkout/success', views.success_view, name="checkout-success"),
     path('checkout/success/<slug:success_id>', views.success_view, name="checkout-success"),
     
-    # Admin section
+    # Activity details pages
+    path('luge', activity_views.LugeView.as_view(), name="luge"),
+
+    # Admin pages
     path('admin/all', admin_views.booking_list_view, name="admin-all"),
     path('admin/edit/<slug:user_slug>/<int:ticket_id>', admin_views.booking_edit_view, name="admin-edit"),
     path('admin/edit/<slug:user_slug>/<int:ticket_id>/activate', admin_views.booking_activate_view, name="admin-edit-activate"),
