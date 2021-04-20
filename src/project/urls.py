9317a695c0bbf30e956fa80de51de84d2ab723ba
Michael <37400755+micha31r@users.noreply.github.com>
2021-04-20 17:43:43 +1200

    Added login and logout page
    and bug fixes

--- src/project/urls.py
+++ src/project/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     # path('404', views.handler404, name="404"),
     # path('500', views.handler500, name="500"),
     path('booking/', include('booking.urls')),
+    path('user/', include('usermgmt.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- src/usermgmt/views.py
+++ src/usermgmt/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import *
 
-# Create your views here.
+def login_view(request):
+	ctx = {} # Context variables
+	next_page = request.GET.get("next") # Get url of the next page
+	default_page = "booking:admin-all"
+	if request.user.is_authenticated:
+		return redirect(next_page or default_page)
+	login_form = LoginForm(request.POST or None)
+	ctx["form"] = login_form
+	if login_form.is_valid():
+		username = login_form.cleaned_data.get("username")
+		password = login_form.cleaned_data.get("password")
+		user = authenticate(request, username=username, password=password)
+		if user and user.is_staff:
+			login(request, user)
+			return redirect(next_page or default_page) # Redirect to the next page
+		else: 
+			messages.add_message(request, messages.ERROR, 'Wrong username or password')
+	login_form = LoginForm()
+	template_file = "usermgmt/login.html"
+	return render(request, template_file, ctx)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("usermgmt:login")
