3cd439c860dc2743f64465b40f32084e08068cfc
Michael <37400755+micha31r@users.noreply.github.com>
2021-05-14 14:54:09 +1200

    Fixed card animation

--- src/project/static/home/main.js
+++ src/project/static/home/main.js
@@ -5,6 +5,17 @@ $(function() {
 			"top": $("nav").outerHeight() + $(".nav-activities").outerHeight() + "px",
 			"height": $(".intro-container-wrapper").outerHeight(),
 		});
+		// Activity cards
+		var max = 0;
+		$(".card").each(function() {
+			let vp = $(this).find(".vp");
+			let val = vp.find("img").outerHeight() + vp.find(".initial").outerHeight() + 20;
+			if (max < val) { 
+				max = val;
+			}
+		});
+		$(".vp").height(max);
+		$(".text-container").outerHeight(max);
 	};
 	$(window).resize(f);
 	f();
