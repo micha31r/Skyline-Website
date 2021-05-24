79950574222152483a33597b594662a77c7f7e47
Michael <37400755+micha31r@users.noreply.github.com>
2021-05-24 20:21:32 +1200

    Edited things

--- src/project/static/base/prompt.js
+++ src/project/static/base/prompt.js
@@ -1,7 +1,9 @@
 $(function() {
+	// Show a pop-up prompt when its trigger is clicked
 	$(".prompt-toggle").each(function() {
 		$(this).click(function() {
 			const id = $(this).prop("value");
+			// Get the correct prompt by id if there are multiple on the page
 			const identifier = "#d-prompt" + (id ? '-'+id : '');
 			var element = $(identifier);
 			if (element.css("display") == "none") {
@@ -9,6 +11,7 @@ $(function() {
 			}
 		});
 	});
+	// Close the prompt if clicked outside of the prompt
 	$(".d-prompt").click(function(e) {
 		if (e.target == this) {
 	        $(".d-prompt").hide();
