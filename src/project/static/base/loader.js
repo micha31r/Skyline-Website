e8e0a878b96cc2f6698f3e19d7ef91670ad6434f
Michael <37400755+micha31r@users.noreply.github.com>
2021-05-24 20:21:32 +1200

    Edited things

--- src/project/static/base/loader.js
+++ src/project/static/base/loader.js
@@ -1,9 +1,11 @@
-document.onreadystatechange = function() { 
+document.onreadystatechange = function() {
+	var bd = document.querySelector("body");
+	var ld = document.querySelector("#loader");
     if (document.readyState !== "complete") { 
-        document.querySelector("body").style.visibility = "hidden"; 
-        document.querySelector("#loader").style.visibility = "visible";
+        bd.style.visibility = "hidden"; 
+        ld.style.visibility = "visible";
     } else { 
-        document.querySelector("#loader").style.display = "none";  
-        document.querySelector("body").style.visibility = "visible"; 
+        ld.style.display = "none";  
+        bd.style.visibility = "visible"; 
     } 
-}; 
+};
